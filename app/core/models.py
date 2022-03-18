cb2840c19c1c57a239ccd54f5e742c78ef4469d8
musleh0001 <khan.muslehuddin0001@gmail.com>
2022-03-18 21:53:43 +0600

    add Tag models

--- app/core/models.py
+++ app/core/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin,
 )
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
@@ -38,3 +39,13 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+
+
+class Tag(models.Model):
+    """Tag to be used for recipe"""
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email="test@musleh.com", password="testpass"):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
@@ -30,3 +37,9 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(user=sample_user(), name="Vegan")
+
+        self.assertEqual(str(tag), tag.name)
